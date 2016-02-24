a9ff98a4b35e9aec20e9fe743b5d9323a648158e
Kevin Wells <kevin.c.wells@intel.com>
2016-02-23 18:42:56 -0800

    Move navigation files and documentation into appropriate location.
    The navigation files were in the wrong spot and would not allow for
    a proper catkin_make. The files have been moved into the correct spot
    and the entire navigation process has been tested.

--- camera/ros-realsense-nav/turtlebot/install_realsense_navigation.sh
+++ camera/ros-realsense-nav/turtlebot/install_realsense_navigation.sh
@@ -6,5 +6,5 @@
 sudo cp ./robot_description/kobuki_minimal_r200.urdf.xacro /opt/ros/$ROS_DISTRO/share/turtlebot_description/robots/
 sudo cp ./robot_description/r200.launch.xml /opt/ros/$ROS_DISTRO/share/turtlebot_bringup/launch/includes/3dsensor/
 
-sudo mkdir /opt/ros/$ROS_DISTRO/share/realsense_navigation
+sudo mkdir -p /opt/ros/$ROS_DISTRO/share/realsense_navigation
 sudo cp -r ./* /opt/ros/$ROS_DISTRO/share/realsense_navigation
