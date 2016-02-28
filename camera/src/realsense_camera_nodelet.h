20f8a4ec389bf24511427f5b0e2226bc1eb80502
Reagan Lopez <reagan.lopez@intel.com>
2016-02-27 19:22:32 -0800

    add enable_tf flag to allow for disabling publish transform

--- camera/src/realsense_camera_nodelet.h
+++ camera/src/realsense_camera_nodelet.h
@@ -8,12 +8,12 @@
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
 
- 2. Redistributions in binary form must reproduce the above copyright notice, 
- this list of conditions and the following disclaimer in the documentation 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
 
- 3. Neither the name of the copyright holder nor the names of its contributors 
- may be used to endorse or promote products derived from this software without 
+ 3. Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
  specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -86,6 +86,7 @@ public:
   const bool ENABLE_DEPTH = true;
   const bool ENABLE_COLOR = true;
   const bool ENABLE_PC = true;
+  const bool ENABLE_TF = true;
   const uint32_t SERIAL_NUMBER = 0xFFFFFFFF;
   const rs_format DEPTH_FORMAT = RS_FORMAT_Z16;
   const rs_format COLOR_FORMAT = RS_FORMAT_RGB8;
@@ -126,6 +127,7 @@ private:
   bool enable_color_;
   bool enable_depth_;
   bool enable_pointcloud_;
+  bool enable_tf_;
   std::vector<std::string> camera_configuration_;
   std::string camera_ = "R200";
   const uint16_t *image_depth16_;
@@ -138,7 +140,7 @@ private:
     RS_OPTION_R200_AUTO_EXPOSURE_RIGHT_EDGE,
     RS_OPTION_R200_AUTO_EXPOSURE_BOTTOM_EDGE
   };
-  double edge_values_[4]; 
+  double edge_values_[4];
 
   sensor_msgs::CameraInfoPtr camera_info_ptr_[STREAM_COUNT];
   sensor_msgs::CameraInfo * camera_info_[STREAM_COUNT];
