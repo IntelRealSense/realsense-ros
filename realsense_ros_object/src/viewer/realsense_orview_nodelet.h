602613f3ed36333db995dcd62de696a546639fb4
Shoshana Skurnik <shoshana.skurnik@intel.com>
2017-02-22 14:20:04 +0200

    Change object sample source code to follow ROS source code guidlines

--- realsense_ros_object/src/viewer/realsense_orview_nodelet.h
+++ realsense_ros_object/src/viewer/realsense_orview_nodelet.h
@@ -10,90 +10,82 @@
 ///////////////////////////////////////////////
 
 #include <nodelet/nodelet.h>
-
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-
 #include "GUI_utils.h"
-
 #include <message_filters/time_synchronizer.h>
 
-//typedef message_filters::TimeSynchronizer<sensor_msgs::Image, realsense_ros_object::ObjectsInBoxes> trackingSynchronizer;
-//typedef message_filters::TimeSynchronizer<sensor_msgs::Image, sie_common::RecogRsObjectTracker> sonySynchronizer;
 
 namespace realsense_ros_object
 {
-	///////////////////////////////////////////////
-	///	CTracking - 
-	///////////////////////////////////////////////
-	class COrView
-	{
-	public :
-		//===================================
-		//	Interface
-		//===================================
-	  	void getParams(ros::NodeHandle& nh);
-		void getParams(const std::vector<std::string> & argv);
-		void init(ros::NodeHandle& nh);
-  
-		COrView();
-		virtual ~COrView();
+///////////////////////////////////////////////
+///	CTracking -
+///////////////////////////////////////////////
+class COrView
+{
+public:
+    //===================================
+    //	Interface
+    //===================================
+    void getParams(ros::NodeHandle& nh);
+    void getParams(const std::vector<std::string> & argv);
+    void init(ros::NodeHandle& nh);
 
-	private:
-		//===================================
-		//	Member Functions
-		//===================================
+    COrView();
+    virtual ~COrView();
 
-		int initialize();
-		int unInitialize();
-		//Static member functions:
-		void localizedTrackedObjectsCallback(const sensor_msgs::ImageConstPtr& color,const realsense_ros_object::ObjectsInBoxes::ConstPtr& msg);
-		
-		//===================================
-		//	Member Variables
-		//===================================
+private:
+    //===================================
+    //	Member Functions
+    //===================================
 
-		std::unique_ptr<message_filters::Subscriber<sensor_msgs::Image>>			mColorSubscriber;
+    int initialize();
+    int unInitialize();
+    //Static member functions:
+    void localizedTrackedObjectsCallback(const sensor_msgs::ImageConstPtr& color,const realsense_ros_object::ObjectsInBoxes::ConstPtr& msg);
 
-		std::unique_ptr<message_filters::Subscriber<realsense_ros_object::ObjectsInBoxes>> 		mTrackingSubscriber;
-		
-		std::unique_ptr<message_filters::TimeSynchronizer<sensor_msgs::Image, realsense_ros_object::ObjectsInBoxes>> mTimeSynchronizer;
-		
-		ros::Publisher m_UI_pub;
-		ros::NodeHandle m_nh;
-		
-		GUI_utils gui;
-		
-		cv::Mat m_imageColor;
+    //===================================
+    //	Member Variables
+    //===================================
 
-	};
-	
+    std::unique_ptr<message_filters::Subscriber<sensor_msgs::Image>> color_subscriber_;
+    std::unique_ptr<message_filters::Subscriber<realsense_ros_object::ObjectsInBoxes>> tracking_subscriber_;
+    std::unique_ptr<message_filters::TimeSynchronizer<sensor_msgs::Image, realsense_ros_object::ObjectsInBoxes>> time_synchronizer_;
+    ros::Publisher UI_pub_;
+    ros::NodeHandle nh_;
+
+    GUI_utils gui;
+
+    cv::Mat m_imageColor;
+
+};
+
 #ifdef RUN_AS_NODELET
-	///////////////////////////////////////////////
-	///	COrViewNodelet 
-	///////////////////////////////////////////////
-	class COrViewNodelet : public nodelet::Nodelet
-	{
-	public:
-		//===================================
-		//	Interface
-		//===================================
-		virtual void onInit();
+///////////////////////////////////////////////
+///	COrViewNodelet
+///////////////////////////////////////////////
+class COrViewNodelet : public nodelet::Nodelet
+{
+public:
+    //===================================
+    //	Interface
+    //===================================
+    virtual void onInit();
 
-		~COrViewNodelet();
+    ~COrViewNodelet();
 
-	private:
-		//===================================
-		//	Member Functions
-		//===================================
+private:
+    //===================================
+    //	Member Functions
+    //===================================
 
-		//===================================
-		//	Member Variables
-		//===================================
+    //===================================
+    //	Member Variables
+    //===================================
 
-		COrView m_ViewNodelet;
+    COrView view_nodelet_;
 
-	};
+};
 #endif
 	
 };
