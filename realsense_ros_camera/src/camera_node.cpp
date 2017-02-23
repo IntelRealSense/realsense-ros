78821b01042d38ba69bf41c3df60216a3d0f4d9a
Matt Curfman <matthew.c.curfman@intel.com>
2017-02-22 16:14:47 -0800

    Added enable/disable stream support, so that SLAM does not run color stream, or OR/PT does not run fisheye stream unnecessarily.  Additional code cleanup/refactoring in camera node

--- realsense_ros_camera/src/camera_node.cpp
+++ realsense_ros_camera/src/camera_node.cpp
@@ -24,20 +24,25 @@
 #include <sensor_msgs/Imu.h>
 
 const int STREAM_COUNT = 5;
-ros::Publisher feInfo_publisher_,
-    colorInfo_publisher_,
-    depthInfo_publisher_,
+ros::Publisher //feInfo_publisher_,
+    //colorInfo_publisher_,
+    //depthInfo_publisher_,
     accelInfo_publisher_,
     gyroInfo_publisher_,
     fe2imu_publisher_,
     fe2depth_publisher_;
-image_transport::Publisher image_publishers_[STREAM_COUNT] = {};
+//image_transport::Publisher image_publishers_[STREAM_COUNT] = {};
+std::map<rs::stream,image_transport::Publisher> image_publishers_;
 ros::Publisher imu_publishers_[2] = {};//accel 0 gyro 1
 int seq_motion[2]= {0,0};
 cv::Mat image_[STREAM_COUNT] = {};
-sensor_msgs::CameraInfo camera_info_[STREAM_COUNT] = {};
+std::map<rs::stream,sensor_msgs::CameraInfo> camera_info_;
 sensor_msgs::ImagePtr image_msg_[STREAM_COUNT] = {};
 int seq[STREAM_COUNT]= {0,0,0,0,0};
+
+std::map<rs::stream,int> image_format_;
+std::map<rs::stream,rs::format> format_;
+std::map<rs::stream,ros::Publisher> info_publisher_;
 
 std::string encoding_[STREAM_COUNT] = {
     sensor_msgs::image_encodings::TYPE_16UC1,
@@ -130,7 +135,16 @@ namespace realsense_ros_camera
     class NodeletCamera:public nodelet::Nodelet
     {
     public:
-        NodeletCamera() {}
+        NodeletCamera() 
+        {
+            image_format_[rs::stream::depth] = CV_16UC1;
+            image_format_[rs::stream::color] = CV_8UC3;
+            image_format_[rs::stream::fisheye] = CV_8UC1;
+
+            format_[rs::stream::depth] = rs::format::z16;
+            format_[rs::stream::color] = rs::format::rgb8;
+            format_[rs::stream::fisheye] = rs::format::raw8;
+        }
 
         virtual ~NodeletCamera()
         {        
@@ -158,14 +172,17 @@ namespace realsense_ros_camera
           pnh_.param("depth_width", width_[rs::stream::depth], DEPTH_WIDTH);
           pnh_.param("depth_height", height_[rs::stream::depth], DEPTH_HEIGHT);
           pnh_.param("depth_fps", fps_[rs::stream::depth], DEPTH_FPS);
+          pnh_.param("enable_depth", enable_[rs::stream::depth], true);
 
           pnh_.param("color_width", width_[rs::stream::color], COLOR_WIDTH);
           pnh_.param("color_height", height_[rs::stream::color], COLOR_HEIGHT);
           pnh_.param("color_fps", fps_[rs::stream::color], COLOR_FPS);
+          pnh_.param("enable_color", enable_[rs::stream::color], false);
 
           pnh_.param("fisheye_width", width_[rs::stream::fisheye], FISHEYE_WIDTH);
           pnh_.param("fisheye_height", height_[rs::stream::fisheye], FISHEYE_HEIGHT);
           pnh_.param("fisheye_fps", fps_[rs::stream::fisheye], FISHEYE_FPS);
+          pnh_.param("enable_fisheye", enable_[rs::stream::fisheye], false);
         }//end getParameters
 
 
@@ -203,6 +220,8 @@ namespace realsense_ros_camera
             ROS_INFO_STREAM(device_name);
             if (std::string(device_name).find("ZR300") == std::string::npos) {
                 isZR300 = false;
+                enable_[rs::stream::fisheye] = false;
+
                 if ((std::string(device_name).find("R200") == std::string::npos) && (std::string(device_name).find("SR300") == std::string::npos))
                 {
                     ROS_INFO_STREAM("Please use ZR300, LR200 or SR300");
@@ -210,7 +229,8 @@ namespace realsense_ros_camera
                 }
             }
             else {
-                isZR300 = true;
+                if( true == enable_[rs::stream::fisheye])
+                    isZR300 = true;
             }    
         }//end setupDevice
 
@@ -219,23 +239,29 @@ namespace realsense_ros_camera
         {
             image_transport::ImageTransport image_transport(getNodeHandle());
 
-            // Stream publishers
-            image_publishers_[(int32_t)rs::stream::color] = image_transport.advertise("camera/color/image_raw", 1);
-            image_publishers_[(int32_t)rs::stream::depth] = image_transport.advertise("camera/depth/image_raw", 1);
+            // Stream publishers and latched topics
+            if(true == enable_[rs::stream::color])
+            {
+                image_publishers_[rs::stream::color] = image_transport.advertise("camera/color/image_raw", 1);
+                info_publisher_[rs::stream::color] = node_handle.advertise< sensor_msgs::CameraInfo >("camera/color/camera_info", 1, true);
+            }
 
-            // Latched topics            
-            colorInfo_publisher_ = node_handle.advertise< sensor_msgs::CameraInfo >("camera/color/camera_info", 1, true);
-            depthInfo_publisher_ = node_handle.advertise< sensor_msgs::CameraInfo >("camera/depth/camera_info", 1, true);
-
+            if(true == enable_[rs::stream::depth])
+            {
+                image_publishers_[rs::stream::depth] = image_transport.advertise("camera/depth/image_raw", 1);
+                info_publisher_[rs::stream::depth] = node_handle.advertise< sensor_msgs::CameraInfo >("camera/depth/camera_info", 1, true);
+            }
+           
             if (isZR300)
             {
                 // Stream publishers
-                image_publishers_[(int32_t)rs::stream::fisheye] = image_transport.advertise("camera/fisheye/image_raw", 1);
+                image_publishers_[rs::stream::fisheye] = image_transport.advertise("camera/fisheye/image_raw", 1);
+                info_publisher_[rs::stream::fisheye] = node_handle.advertise< sensor_msgs::CameraInfo >("camera/fisheye/camera_info", 1, true);
+
                 imu_publishers_[RS_EVENT_IMU_GYRO] = node_handle.advertise< sensor_msgs::Imu >("camera/gyro/sample", 100); // TODO: review queue size
                 imu_publishers_[RS_EVENT_IMU_ACCEL] = node_handle.advertise< sensor_msgs::Imu >("camera/accel/sample", 100); // TODO: review queue size
                 
                 // Latched topics            
-                feInfo_publisher_ = node_handle.advertise< sensor_msgs::CameraInfo >("camera/fisheye/camera_info", 1, true);
                 fe2imu_publisher_ = node_handle.advertise< Extrinsics >("camera/extrinsics/fisheye2imu", 1, true);
                 fe2depth_publisher_ = node_handle.advertise< Extrinsics >("camera/extrinsics/fisheye2depth", 1, true);
                 accelInfo_publisher_ = node_handle.advertise< IMUInfo >("camera/accel/imu_info", 1, true);
@@ -246,23 +272,14 @@ namespace realsense_ros_camera
 
         void setupStreams()
         {
-            std::map< rs::stream, std::function< void (rs::frame) > > stream_callback_per_stream;
             const rs::stream All[] = { rs::stream::depth, rs::stream::color, rs::stream::fisheye };
 
             for ( const auto stream : All )
             {
-                if (stream == rs::stream::depth)
-                {
-                    device->enable_stream(stream, width_[stream], height_[stream], rs::format::z16, fps_[stream]);
-                }
-                else if (stream == rs::stream::fisheye && isZR300)
-                {
-                    device->enable_stream(stream, width_[stream], height_[stream], rs::format::raw8, fps_[stream]);
-                }
-                else if (stream == rs::stream::color)
-                {
-                device->enable_stream(stream, width_[stream], height_[stream], rs::format::rgb8, fps_[stream]);
-                }
+                if( false == enable_[stream])
+                    continue;
+
+                image_[(int32_t)stream] = cv::Mat(camera_info_[stream].height, camera_info_[stream].width, image_format_[stream], cv::Scalar(0,0,0));
 
                 stream_callback_per_stream[stream] = [stream](rs::frame frame)
                 {
@@ -288,20 +305,28 @@ namespace realsense_ros_camera
                     image_msg_[stream_nb]->step = image_[stream_nb].cols * unit_step_size_[stream_nb];
                     seq[stream_nb] += 1;
 
-                    image_publishers_[stream_nb].publish(image_msg_[stream_nb]);
+                    image_publishers_[stream].publish(image_msg_[stream_nb]);
                 };
+
+                device->enable_stream(stream, width_[stream], height_[stream], format_[stream], fps_[stream]);
                 device->set_frame_callback(stream, stream_callback_per_stream[stream]);
+
+                // Publish stream info
+                getStreamCalibData(stream);
+                info_publisher_[stream].publish(camera_info_[stream]);
             }//end for
             
+
             if(isZR300) {
                 device->set_option(rs::option::fisheye_strobe, 1);
+
                 //define callback to the motion events and set it.
                 std::function<void(rs::motion_data)> motion_callback;
-                std::function<void(rs::timestamp_data)> timestamp_callback;
                 motion_callback = [](rs::motion_data entry)
                 {
                     if ((entry.timestamp_data.source_id != RS_EVENT_IMU_GYRO) && (entry.timestamp_data.source_id != RS_EVENT_IMU_ACCEL))
                         return;
+
                     rs_event_source motionType = entry.timestamp_data.source_id;
                     sensor_msgs::Imu imu_msg = sensor_msgs::Imu();
                     imu_msg.header.stamp = ros::Time::now();
@@ -326,32 +351,22 @@ namespace realsense_ros_camera
                     imu_msg.header.stamp = ros::Time(entry.timestamp_data.timestamp);
                     imu_publishers_[motionType].publish(imu_msg);
                 };
+                
+                std::function<void(rs::timestamp_data)> timestamp_callback;
                 timestamp_callback = [](rs::timestamp_data entry) {};
+                
                 device->enable_motion_tracking(motion_callback, timestamp_callback);
-                getStreamCalibData(rs::stream::fisheye);
-                image_[(int32_t)rs::stream::fisheye] = cv::Mat(camera_info_[(int32_t)rs::stream::fisheye].height, camera_info_[(int32_t)rs::stream::fisheye].width, CV_8UC1, cv::Scalar(0,0,0));
                         
                 // publish ZR300-specific intrinsics/extrinsics
                 IMUInfo accelInfo, gyroInfo;
                 getImuInfo(device, accelInfo, gyroInfo);
-                feInfo_publisher_.publish(camera_info_[(int32_t)rs::stream::fisheye]);
+                
                 fe2imu_publisher_.publish(getFisheye2ImuExtrinsicsMsg(device));
                 fe2depth_publisher_.publish(getFisheye2DepthExtrinsicsMsg(device));
                 accelInfo_publisher_.publish(accelInfo);
                 gyroInfo_publisher_.publish(gyroInfo);
             }
-            
-            // get camera intrinsics
-            getStreamCalibData(rs::stream::depth);
-            getStreamCalibData(rs::stream::color);
-            
-            // publish camera intrinsics/extrinsics        
-            colorInfo_publisher_.publish(camera_info_[(int32_t)rs::stream::color]);
-            depthInfo_publisher_.publish(camera_info_[(int32_t)rs::stream::depth]);
-            
-            image_[(int32_t)rs::stream::depth]   = cv::Mat(camera_info_[(int32_t)rs::stream::depth].height, camera_info_[(int32_t)rs::stream::depth].width, CV_16UC1, cv::Scalar(0,0,0));
-            image_[(int32_t)rs::stream::color]   = cv::Mat(camera_info_[(int32_t)rs::stream::color].height, camera_info_[(int32_t)rs::stream::color].width, CV_8UC3, cv::Scalar(0,0,0));
-
+                        
             if(isZR300)
                 device->start(rs::source::all_sources);
             else
@@ -359,11 +374,10 @@ namespace realsense_ros_camera
         }//end setupStreams
 
 
-        void getStreamCalibData(rs::stream stream_index)
+        void getStreamCalibData(rs::stream stream)
         {
-            rs::intrinsics intrinsic = device->get_stream_intrinsics(stream_index);
-            int32_t stream = (int32_t)stream_index;
-            camera_info_[stream].header.frame_id = optical_frame_id_[stream];
+            rs::intrinsics intrinsic = device->get_stream_intrinsics(stream);
+            camera_info_[stream].header.frame_id = optical_frame_id_[(int32_t)stream];
             camera_info_[stream].width = intrinsic.width;
             camera_info_[stream].height = intrinsic.height;
 
@@ -373,20 +387,20 @@ namespace realsense_ros_camera
             camera_info_[stream].K.at(5) = intrinsic.ppy;
             camera_info_[stream].K.at(8) = 1;
 
-            camera_info_[stream].P.at(0) = camera_info_->K.at(0);
+            camera_info_[stream].P.at(0) = camera_info_[stream].K.at(0);
             camera_info_[stream].P.at(1) = 0;
-            camera_info_[stream].P.at(2) = camera_info_->K.at(2);
+            camera_info_[stream].P.at(2) = camera_info_[stream].K.at(2);
             camera_info_[stream].P.at(3) = 0;
             camera_info_[stream].P.at(4) = 0;
-            camera_info_[stream].P.at(5) = camera_info_->K.at(4);
-            camera_info_[stream].P.at(6) = camera_info_->K.at(5);
+            camera_info_[stream].P.at(5) = camera_info_[stream].K.at(4);
+            camera_info_[stream].P.at(6) = camera_info_[stream].K.at(5);
             camera_info_[stream].P.at(7) = 0;
             camera_info_[stream].P.at(8) = 0;
             camera_info_[stream].P.at(9) = 0;
             camera_info_[stream].P.at(10) = 1;
             camera_info_[stream].P.at(11) = 0;
             
-            if (stream_index == rs::stream::depth)
+            if (stream == rs::stream::depth)
             {
                 // set depth to color translation values in Projection matrix (P)
                 rs::extrinsics extrinsic = device->get_extrinsics(rs::stream::depth, rs::stream::color);
@@ -397,8 +411,6 @@ namespace realsense_ros_camera
                 for (int i = 0; i < 9; i++)
                     camera_info_[stream].R.at(i) = extrinsic.rotation[i];
             }
-            
-            ROS_INFO_STREAM(" model "<<(int32_t)intrinsic.model()<<" stream index "<<stream_index);
             
             switch((int32_t)intrinsic.model())
             {
@@ -420,7 +432,7 @@ namespace realsense_ros_camera
             }
             
             // set R (rotation matrix) values to identity matrix
-            if (stream_index != rs::stream::depth)
+            if (stream != rs::stream::depth)
             {
                 camera_info_[stream].R.at(0) = 1.0;
                 camera_info_[stream].R.at(1) = 0.0;
@@ -450,8 +462,11 @@ namespace realsense_ros_camera
         std::map<rs::stream,int> width_;
         std::map<rs::stream,int> height_;
         std::map<rs::stream,int> fps_;
+        std::map<rs::stream,bool> enable_;
 
-        std::unique_ptr< rs::context > ctx;        
+        std::unique_ptr< rs::context > ctx; 
+
+        std::map< rs::stream, std::function< void (rs::frame) > > stream_callback_per_stream;       
     };//end class
 
 PLUGINLIB_DECLARE_CLASS(realsense_ros_camera, NodeletCamera, realsense_ros_camera::NodeletCamera, nodelet::Nodelet);
