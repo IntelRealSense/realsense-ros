8ddfc42cf56dd9d8161cbc9c21d82c984a011df3
Matt Curfman <matthew.c.curfman@intel.com>
2017-03-14 16:26:39 -0700

    Cleanup changes 1. Set correct copyright date of 2017:    find . -name *.cpp -exec sed -i -e 's#(c) 2016#(c) 2017#g' {} \;    find . -name *.h -exec sed -i -e 's#(c) 2016#(c) 2017#g' {} \; 2. Removed a .swp file somebody checked in by mistake 3. Removed release note file in Person Tracking: should using CHANGELOG.rst in ROS format 4. Fixed compile warning of broken dependency in SLAM CMake file

--- realsense_ros_person/src/sample/TrackingRenderer/Colors.h
+++ realsense_ros_person/src/sample/TrackingRenderer/Colors.h
@@ -1,6 +1,6 @@
 
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2016 Intel Corporation. All Rights Reserved#pragma once
+// Copyright(c) 2017 Intel Corporation. All Rights Reserved#pragma once
 
 #include <opencv2/core/core.hpp>
 
