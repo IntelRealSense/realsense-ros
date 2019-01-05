af014863ac3f0838550eb8659890875d99f15807
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2019-01-05 01:46:22 -0800

    [dep install script] Common command as a variable.

--- .install_dependency.sh
+++ .install_dependency.sh
@@ -6,8 +6,9 @@
 # Codes taken from https://github.com/intel-ros/realsense/blob/c5ea27245967e0938f7d10384f4b7279e01000b4/.travis.yml
 # When used on CI sudo is not usually needed but it is most likely needed when used on your local computer.
 
-echo "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/realsense-public.list
-add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -sc) main"
+COMMAND="deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -sc) main"
+echo $COMMAND | tee /etc/apt/sources.list.d/realsense-public.list
+add-apt-repository "$COMMAND"
 apt-get update -qq
 apt-get install librealsense2-dkms --allow-unauthenticated -y 
 apt-get install librealsense2-dev --allow-unauthenticated -y
