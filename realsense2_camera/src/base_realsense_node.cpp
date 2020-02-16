a35401ac8cff486312129d32b6edf0a10d9578cf
doronhi <doron.hirshberg@intel.com>
2020-02-16 12:24:29 +0200

    add error message "Left IC2 Config error" to reset inducing errors list.

--- realsense2_camera/src/base_realsense_node.cpp
+++ realsense2_camera/src/base_realsense_node.cpp
@@ -165,7 +165,8 @@ void BaseRealSenseNode::setupErrorCallback()
     {
         s.set_notifications_callback([&](const rs2::notification& n)
         {
-            std::vector<std::string> error_strings({"RT IC2 Config error", 
+            std::vector<std::string> error_strings({"Left IC2 Config error",
+                                                    "RT IC2 Config error", 
                                                     "Motion Module force pause",
                                                     "stream start failure"});
             if (n.get_severity() >= RS2_LOG_SEVERITY_ERROR)
