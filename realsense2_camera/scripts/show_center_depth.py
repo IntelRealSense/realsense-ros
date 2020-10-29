6a156d76b1226782ebb3f88f672dd751d8c0a1a9
doronhi <doron_hirshberg@hotmail.com>
2020-10-29 09:44:16 +0200

    Fix show_center_depth.py to use intrinsics from camera_info topic and calculate 3D position

--- realsense2_camera/scripts/show_center_depth.py
+++ realsense2_camera/scripts/show_center_depth.py
@@ -1,28 +1,72 @@
 import rospy
 from sensor_msgs.msg import Image as msg_Image
+from sensor_msgs.msg import CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 import sys
 import os
+import pyrealsense2 as rs2
+import numpy as np
 
 class ImageListener:
-    def __init__(self, topic):
-        self.topic = topic
+    def __init__(self, depth_image_topic, depth_info_topic):
         self.bridge = CvBridge()
-        self.sub = rospy.Subscriber(topic, msg_Image, self.imageDepthCallback)
+        self.sub = rospy.Subscriber(depth_image_topic, msg_Image, self.imageDepthCallback)
+        self.sub_info = rospy.Subscriber(depth_info_topic, CameraInfo, self.imageDepthInfoCallback)
+        self.intrinsics = None
 
     def imageDepthCallback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
-            pix = (data.width/2, data.height/2)
-            sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
+            # pick one pixel among all the pixels with the closest range:
+            indices = np.array(np.where(cv_image == cv_image[cv_image > 0].min()))[:,0]
+            pix = (indices[1], indices[0])
+            if self.intrinsics:
+                depth = cv_image[pix[1], pix[0]]
+                result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[0], pix[1]], depth)
+                sys.stdout.write('\rDepth at pixel(%3d, %3d): %4f(mm). Coordinate: %8.2f %8.2f %8.2f\r' % (pix[0], pix[1], cv_image[pix[1], pix[0]], result[0], result[1], result[2]))
+            else:
+                sys.stdout.write('\rDepth at pixel(%3d, %3d): %4f(mm).\r' % (pix[0], pix[1], cv_image[pix[1], pix[0]]))
             sys.stdout.flush()
+
+        except CvBridgeError as e:
+            print(e)
+            return
+        except ValueError as e:
+            return
+
+    def imageDepthInfoCallback(self, cameraInfo):
+        try:
+            if self.intrinsics:
+                return
+            self.intrinsics = rs2.intrinsics()
+            self.intrinsics.width = cameraInfo.width
+            self.intrinsics.height = cameraInfo.height
+            self.intrinsics.ppx = cameraInfo.K[2]
+            self.intrinsics.ppy = cameraInfo.K[5]
+            self.intrinsics.fx = cameraInfo.K[0]
+            self.intrinsics.fy = cameraInfo.K[4]
+            if cameraInfo.distortion_model == 'plumb_bob':
+                self.intrinsics.model = rs2.distortion.brown_conrady
+            elif cameraInfo.distortion_model == 'equidistant':
+                self.intrinsics.model = rs2.distortion.kannala_brandt4
+            self.intrinsics.coeffs = [i for i in cameraInfo.D]
         except CvBridgeError as e:
             print(e)
             return
 
 def main():
-    topic = '/camera/depth/image_rect_raw'
-    listener = ImageListener(topic)
+    depth_image_topic = '/camera/depth/image_rect_raw'
+    depth_info_topic = '/camera/depth/camera_info'
+
+    print ('show_center_depth.py')
+    print ('--------------------')
+    print ('App to demontrate the usage of the /camera/depth topics.')
+    print
+    print ('Application subscribes to %s and %s topics.' % (depth_image_topic, depth_info_topic))
+    print ('Application then calculates and print the range and 3D location of the closest object.')
+    print
+    
+    listener = ImageListener(depth_image_topic, depth_info_topic)
     rospy.spin()
 
 if __name__ == '__main__':
