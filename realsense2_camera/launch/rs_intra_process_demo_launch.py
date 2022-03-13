421ee3eeb1357a18b24c2754dd418df38eceedbf
NirAz <nir.azkiel@intel.com>
2022-03-13 17:23:27 +0200

    verify required components exist on launch file

--- realsense2_camera/launch/rs_intra_process_demo_launch.py
+++ realsense2_camera/launch/rs_intra_process_demo_launch.py
@@ -4,14 +4,31 @@
 '''
 Launch realsense2_camera node & a frame latency printer node, 
 This tool allow the user the evaluate the reduction of the frame latency when intra-process communication is used.
-Note: currently default for color stream only
 Run syntax: ros2 launch realsense2_camera rs_intra_process_demo_launch.py intra_process_comms:=true
+Note: 
+*   Running this tool require building with build tools flag on (colcon build --cmake-args '-DBUILD_TOOLS=ON')
+*   Currently default for color stream only
 '''
+import sys
+import subprocess
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+
+# Make sure required packages can be found
+process = subprocess.run(['ros2','component', 'types'],
+                         stdout=subprocess.PIPE, 
+                         universal_newlines=True)
+
+rs_node_class=  'RealSenseNodeFactory'
+rs_latency_tool_class = 'FrameLatencyNode'
+
+if process.stdout.find(rs_node_class) == -1 or process.stdout.find(rs_latency_tool_class) == -1 :
+    sys.exit('Cannot locate all required node components (' + rs_node_class + ',' + rs_latency_tool_class + ') on the available component list\n' + process.stdout + \
+    '\nplease make sure you run "colcon build --cmake-args \'-DBUILD_TOOLS=ON\'" command before running this launch file')
+
 
 configurable_parameters = [{'name': 'camera_name',                  'default': 'camera', 'description': 'camera unique name'},
                            {'name': 'serial_no',                    'default': "''", 'description': 'choose device by serial number'},
@@ -47,14 +64,14 @@ def generate_launch_description():
                 ComposableNode(
                     package='realsense2_camera',
                     namespace='',
-                    plugin='realsense2_camera::RealSenseNodeFactory',
+                    plugin='realsense2_camera::' + rs_node_class,
                     name="camera",
                     parameters=[set_configurable_parameters(configurable_parameters)],
                     extra_arguments=[{'use_intra_process_comms': LaunchConfiguration("intra_process_comms")}]) ,
                 ComposableNode(
                     package='realsense2_camera',
                     namespace='',
-                    plugin='rs2_ros::tools::frame_latency::FrameLatencyNode',
+                    plugin='rs2_ros::tools::frame_latency::' + rs_latency_tool_class,
                     name='frame_latency',
                     parameters=[set_configurable_parameters(configurable_parameters)],
                     extra_arguments=[{'use_intra_process_comms': LaunchConfiguration("intra_process_comms")}]) ,
