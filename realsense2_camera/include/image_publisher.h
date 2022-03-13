f79d814505e559879c91302ca9ecc3c878806ec8
Nir Azkiel <nir.azkiel@intel.com>
2022-03-13 11:16:15 +0200

    clang format image_publisher files

--- realsense2_camera/include/image_publisher.h
+++ realsense2_camera/include/image_publisher.h
@@ -6,41 +6,47 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
-#if defined(DASHING) || defined(ELOQUENT)
+#if defined( DASHING ) || defined( ELOQUENT )
 #include <image_transport/image_transport.h>
 #else
 #include <image_transport/image_transport.hpp>
 #endif
 
-namespace realsense2_camera
+namespace realsense2_camera {
+class image_publisher
 {
-    class image_publisher
-    {
-    public:
-        virtual void publish(sensor_msgs::msg::Image::UniquePtr image_ptr) = 0;
-        virtual size_t get_subscription_count() const = 0;
-        virtual ~image_publisher() = default;
-    };
+public:
+    virtual void publish( sensor_msgs::msg::Image::UniquePtr image_ptr ) = 0;
+    virtual size_t get_subscription_count() const = 0;
+    virtual ~image_publisher() = default;
+};
 
-    class image_rcl_publisher : public image_publisher
-    {
-    public:
-        image_rcl_publisher(rclcpp::Node &node, const std::string &topic_name, const rmw_qos_profile_t &qos);
-        void publish(sensor_msgs::msg::Image::UniquePtr image_ptr) override;
-        size_t get_subscription_count() const override; 
+// Native RCL implementation of an image publisher (needed for intra-process communication)
+class image_rcl_publisher : public image_publisher
+{
+public:
+    image_rcl_publisher( rclcpp::Node & node,
+                         const std::string & topic_name,
+                         const rmw_qos_profile_t & qos );
+    void publish( sensor_msgs::msg::Image::UniquePtr image_ptr ) override;
+    size_t get_subscription_count() const override;
 
-    private:
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_impl;
-    };
+private:
+    rclcpp::Publisher< sensor_msgs::msg::Image >::SharedPtr image_publisher_impl;
+};
 
-    class image_transport_publisher : public image_publisher
-    {
-    public:
-        image_transport_publisher(rclcpp::Node &node, const std::string &topic_name, const rmw_qos_profile_t &qos);
-        void publish(sensor_msgs::msg::Image::UniquePtr image_ptr) override;
-        size_t get_subscription_count() const override; 
-    private:
-        std::shared_ptr<image_transport::Publisher> image_publisher_impl;
-    };
+// image_transport implementation of an image publisher (adds a compressed image topic)
+class image_transport_publisher : public image_publisher
+{
+public:
+    image_transport_publisher( rclcpp::Node & node,
+                               const std::string & topic_name,
+                               const rmw_qos_profile_t & qos );
+    void publish( sensor_msgs::msg::Image::UniquePtr image_ptr ) override;
+    size_t get_subscription_count() const override;
 
-}
+private:
+    std::shared_ptr< image_transport::Publisher > image_publisher_impl;
+};
+
+}  // namespace realsense2_camera
