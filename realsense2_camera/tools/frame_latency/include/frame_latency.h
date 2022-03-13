73bb2a2ba6d5b5e7616b750b3962358f4e60d387
Nir Azkiel <nir.azkiel@intel.com>
2022-03-13 11:20:11 +0200

    clang format frame_latency files

--- realsense2_camera/tools/frame_latency/include/frame_latency.h
+++ realsense2_camera/tools/frame_latency/include/frame_latency.h
@@ -6,24 +6,24 @@
 #include <rclcpp/rclcpp.hpp>
 #include "sensor_msgs/msg/image.hpp"
 
-namespace rs2_ros
+namespace rs2_ros {
+namespace tools {
+namespace frame_latency {
+class FrameLatencyNode : public rclcpp::Node
 {
-    namespace tools
-    {
-        namespace frame_latency
-        {
-            class FrameLatencyNode : public rclcpp::Node
-            {
-            public:
-                explicit FrameLatencyNode(const rclcpp::NodeOptions &node_options = rclcpp::NodeOptions().use_intra_process_comms(true));
+public:
+    explicit FrameLatencyNode( const rclcpp::NodeOptions & node_options
+                               = rclcpp::NodeOptions().use_intra_process_comms( true ) );
 
-                FrameLatencyNode(const std::string &node_name, const std::string &ns, const rclcpp::NodeOptions &node_options = rclcpp::NodeOptions().use_intra_process_comms(true));
+    FrameLatencyNode( const std::string & node_name,
+                      const std::string & ns,
+                      const rclcpp::NodeOptions & node_options
+                      = rclcpp::NodeOptions().use_intra_process_comms( true ) );
 
-            private:
-                rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
-                rclcpp::Logger _logger;
-            };
-        }
-    }
-
-}
+private:
+    rclcpp::Subscription< sensor_msgs::msg::Image >::SharedPtr sub_;
+    rclcpp::Logger _logger;
+};
+}  // namespace frame_latency
+}  // namespace tools
+}  // namespace rs2_ros
--- realsense2_camera/tools/frame_latency/src/frame_latency.cpp
+++ realsense2_camera/tools/frame_latency/src/frame_latency.cpp
@@ -10,31 +10,36 @@
 
 using namespace rs2_ros::tools::frame_latency;
 
-FrameLatencyNode::FrameLatencyNode(const std::string & node_name, const std::string & ns,
-										   const rclcpp::NodeOptions & node_options) : 
-	Node(node_name, ns, node_options),
-	_logger(this->get_logger())
+FrameLatencyNode::FrameLatencyNode( const std::string & node_name,
+                                    const std::string & ns,
+                                    const rclcpp::NodeOptions & node_options )
+    : Node( node_name, ns, node_options )
+    , _logger( this->get_logger() )
 {
 }
 
 
-FrameLatencyNode::FrameLatencyNode(const rclcpp::NodeOptions & node_options)
-    : Node("frame_latency", "/", node_options),
-      _logger(this->get_logger())
+FrameLatencyNode::FrameLatencyNode( const rclcpp::NodeOptions & node_options )
+    : Node( "frame_latency", "/", node_options )
+    , _logger( this->get_logger() )
 {
-    ROS_INFO_STREAM("frame_latency node is UP!");
-    ROS_INFO_STREAM("Intra-Process is " << (this->get_node_options().use_intra_process_comms() ? "ON" : "OFF"));
+    ROS_INFO_STREAM( "frame_latency node is UP!" );
+    ROS_INFO_STREAM( "Intra-Process is "
+                     << ( this->get_node_options().use_intra_process_comms() ? "ON" : "OFF" ) );
     // Create a subscription on the input topic.
-    sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-        "/color/image_raw", // TODO Currently color only, we can declare and accept the required streams as ros parameters
-        rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default),rmw_qos_profile_default),
-        [&, this](const sensor_msgs::msg::Image::SharedPtr msg)
-        {
+    sub_ = this->create_subscription< sensor_msgs::msg::Image >(
+        "/color/image_raw",  // TODO Currently color only, we can declare and accept the required
+                             // streams as ros parameters
+        rclcpp::QoS( rclcpp::QoSInitialization::from_rmw( rmw_qos_profile_default ),
+                     rmw_qos_profile_default ),
+        [&, this]( const sensor_msgs::msg::Image::SharedPtr msg ) {
             rclcpp::Time curr_time = this->get_clock()->now();
-            auto latency = (curr_time - msg->header.stamp).seconds();
-            ROS_INFO_STREAM("Got msg with address 0x" << std::hex << reinterpret_cast<std::uintptr_t>(msg.get()) << std::dec << " with latency of " << latency  << " [sec]");
-        });
+            auto latency = ( curr_time - msg->header.stamp ).seconds();
+            ROS_INFO_STREAM( "Got msg with address 0x"
+                             << std::hex << reinterpret_cast< std::uintptr_t >( msg.get() )
+                             << std::dec << " with latency of " << latency << " [sec]" );
+        } );
 }
 
 #include "rclcpp_components/register_node_macro.hpp"
-RCLCPP_COMPONENTS_REGISTER_NODE(rs2_ros::tools::frame_latency::FrameLatencyNode)
+RCLCPP_COMPONENTS_REGISTER_NODE( rs2_ros::tools::frame_latency::FrameLatencyNode )
